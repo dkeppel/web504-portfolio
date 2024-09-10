d89aae5041253adb4e0d6078aa564ca5f3a50126
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2024-09-11 10:58:35 +1200

    document.getElementById Stuff

---
+++ portfolio-project/components/home/home.js
@@ -0,0 +1,7 @@
+// Retrieves elements from HTML by there ID so that they can be further manipulated by the JS
+const model = document.getElementById("model");
+const modelClose = document.getElementById("model-close");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarkContainer = document.getElementById("bookmark-container"); 
