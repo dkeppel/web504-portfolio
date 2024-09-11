7d45d68ee3b87fb92fe955cb3c5b5d655e65b152
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2024-09-12 09:26:27 +1200

    Bookmark thingy

--- portfolio-project/components/home/home.js
+++ portfolio-project/components/home/home.js
@@ -1,7 +1,19 @@
-// Retrieves elements from HTML by there ID so that they can be further manipulated by the JS
+// Declaring constant variables, Retrieves elements from HTML by there ID so that they can be further manipulated by the JS
 const model = document.getElementById("model");
+const showModel = document.getElementById("show-model");
 const modelClose = document.getElementById("model-close");
 const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarkContainer = document.getElementById("bookmark-container"); 
+
+let bookmarks = [];
+
+function showModel() {
+    model.classList.add("show-model")
+    websiteNameEl.focus()
+}
+
+modelClose.addEventListener("click", () => {
+    model.classList.remove("show-model")
+});
