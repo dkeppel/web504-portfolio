05a27d89290b526e7ddd0f9e1693d5ccb570afd0
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2024-09-12 10:41:07 +1200

    Regex

--- portfolio-project/components/home/home.js
+++ portfolio-project/components/home/home.js
@@ -1,6 +1,6 @@
 // Declaring constant variables, Retrieves elements from HTML by there ID so that they can be further manipulated by the JS
 const model = document.getElementById("model");
-const showModel = document.getElementById("show-model");
+const modelShow = document.getElementById("show-model");
 const modelClose = document.getElementById("model-close");
 const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
@@ -9,11 +9,28 @@ const bookmarkContainer = document.getElementById("bookmark-container");
 
 let bookmarks = [];
 
-function showModel() {
+function modelShow() {
     model.classList.add("show-model")
     websiteNameEl.focus()
-}
+};
+
+modelShow.addEventListener("click", showModel);
+
+modelClose.addEventListener("click", () => {
+    modelClose.classList.remove("show-model")
+});
+
+window.addEventListener("click", (e) => {
+    e.target === model ? model.classList.remove("show-model") : false
+});
 
 modelClose.addEventListener("click", () => {
     model.classList.remove("show-model")
 });
+
+function validate(nameValue, urlValue) {
+    const expression =
+        /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=*^]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+        
+    const regex 
+};
